047f81eff7e631a981c1febbf9f3c64d999dc936
Poomsak <deadknight009@gmail.com>
2018-01-11 15:57:59 +0700

    P : support multiscreen

--- app/src/main/java/mdev/mlaocthtione/Fragment/PageLogin.java
+++ app/src/main/java/mdev/mlaocthtione/Fragment/PageLogin.java
@@ -272,7 +272,7 @@ public class PageLogin extends Fragment {
                     }else {
                         params_login.add(new BasicNameValusPostOkHttp().BasicNameValusPostOkHttp("server", "zx"));
                     }
-                    return allCommand.POST_OK_HTTP_SendData("MyUrl.php", params_login);
+                    return allCommand.POST_OK_HTTP_SendData("My_url.php", params_login);
                 }
 
                 @SuppressLint("MissingPermission")
